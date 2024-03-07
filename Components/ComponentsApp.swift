f67eedde14e0feaba4a433f367fd357e17d75499
Lasitha Weligampola <lasiprabo@gmail.com>
2024-03-07 18:08:01 -0500

    Initial Commit

---
+++ Components/ComponentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComponentsApp.swift
+//  Components
+//
+//  Created by Lasitha Weligampola on 2024-03-07.
+//
+
+import SwiftUI
+
+@main
+struct ComponentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Components/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Components
+//
+//  Created by Lasitha Weligampola on 2024-03-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
